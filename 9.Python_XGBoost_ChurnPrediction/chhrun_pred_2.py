f446554da6f7f6c5e8892d009566dd457550f67f
nm <nyan.min-1@kbzbank.com>
2024-12-22 10:18:13 +0630

    fix deprecation warning
    The use_column_width parameter has been deprecated and will be removed in a future release. Please utilize the use_container_width parameter instead.

--- 9.Python_XGBoost_ChurnPrediction/chhrun_pred_2.py
+++ 9.Python_XGBoost_ChurnPrediction/chhrun_pred_2.py
@@ -47,7 +47,7 @@ default_values = [
 ]
 
 # Sidebar setup
-st.sidebar.image("Pic 1.png", use_column_width=True)  # Display Pic 1
+st.sidebar.image("Pic 1.png", use_container_width=True)  # Display Pic 1
 st.sidebar.header("User Inputs")
 
 # Collect user inputs
@@ -72,7 +72,7 @@ input_data_scaled = input_data.copy()
 input_data_scaled[scale_vars] = scaler.transform(input_data[scale_vars])
 
 # App Header
-st.image("Pic 2.png", use_column_width=True)  # Display Pic 2
+st.image("Pic 2.png", use_container_width=True)  # Display Pic 2
 st.title("Customer Churn Prediction")
 
 # Page Layout
